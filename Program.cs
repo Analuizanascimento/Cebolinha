1abe8f45d840a73984cfacc65e1ba209d4a1ee49
Ana Luiza <analuizagoncalvesdonascimento@gmail.com>
2022-04-29 15:54:18 -0300

    finaliza tarefa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fraseDigitada, fraseComDislalia;
+            
+            Console.Write("Digite uma frase: ");
+            fraseDigitada = Console.ReadLine();
+
+            fraseComDislalia = fraseDigitada
+                .Replace("r", "l")
+                .Replace("R", "L");
+
+            Console.WriteLine(fraseComDislalia);
+        }
+    }
+}
+
